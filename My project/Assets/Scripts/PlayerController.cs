6d809a77d872e0930854eb322996c9932fbb9abc
PaulHaardt <97391871+PaulHaardt@users.noreply.github.com>
2022-01-11 09:49:43 +0100

    truc unity #1
    truc unity

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 20f;
+
+    private PlayerMotor motor;
+
+    void Start ()
+    {
+        motor = GetComponent<PlayerMotor>();
+         
+    }
+
+    void Update ()
+    {
+        // Calcuate movement velocity as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        // Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        // Apply movement vector
+        motor.Move(_velocity);
+
+        // Calculates rotation as a 3D vector
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        // Apply rotation
+        motor.Rotate(_rotation);
+
+        // Calculates CAMERA rotation as a 3D vector
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        // Apply rotation for camera
+        motor.RotateCamera(_cameraRotation);
+
+    }
+
+}
