a1abbdb77a2c83d454f0c30f1a72b54814eddda8
dausyla <dausyla@gmail.com>
2022-02-03 17:17:18 +0100

    new kind of generation for trees

--- Assets/Scripts/MainGame/map.cs
+++ Assets/Scripts/MainGame/map.cs
@@ -8,134 +8,88 @@ using Random = UnityEngine.Random;
 public class map : MonoBehaviour
 {
     public GameObject tree1;
-    public GameObject tree2;
-    public GameObject bush1;
-    static List<Vector2> allTrees = new List<Vector2> { };
-    static List<Vector2> allBushes = new List<Vector2> { };
-    public int seed ;
+        public GameObject tree2;
+        public GameObject village;
+        public int seed ;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        Random.seed = seed;
-        float x;
-        float y;
-        for (int i = 0; i < 10; i++)
+        // Start is called before the first frame update
+        void Start()
         {
-            x = 0;
-            y = 0;
-            SeedTree(x,y);
-            SeedBush(x+3, y);
+            Random.seed = seed;
+            Vector3 villagePosition = Random.insideUnitSphere * 150;
+            villagePosition.y = 0;
+            Instantiate(village, villagePosition, Quaternion.identity);
+            
+            trees();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
-    private void SeedTree(float x, float y)
-    {
-        if (PosTreeValid(x, y))
-        {
-            float addX = 0;
-            float addY = 0;
-            GameObject theTree = Instantiate(RandomTree(), new Vector3(x, 0, y), Quaternion.identity);
-            theTree.transform.localScale = new Vector3(Random.Range(1f, 1.6f), Random.Range(1, 1.6f),
-                Random.Range(1, 1.6f));
-            theTree.transform.rotation =RandomRotation();
-            allTrees.Add(new Vector2(x, y));
-            for (int i = 0; i < 4; i++)
-            {
-                addX = x;
-                addY = y;
-                RandomXY(ref addX,ref addY, 5);
-                SeedTree(x+addX, y+addY);
-            }
-        }
-    }
     
-    private void SeedBush(float x, float y)
-    {
-        float addX = 0;
-        float addY = 0;
-        GameObject theBush = Instantiate(bush1, new Vector3(x, 0, y), Quaternion.identity);
-        theBush.transform.localScale = new Vector3(Random.Range(1f, 1.6f), Random.Range(1f, 1.6f),
-            Random.Range(1f, 1.6f));
-        theBush.transform.rotation =RandomRotation();
-        allBushes.Add(new Vector2(x, y));
-        for (int i = 0; i < 4; i++)
+        // Update is called once per frame
+        void Update()
         {
-            addX = x;
-            addY = y;
-            RandomXY(ref addX,ref addY, 10);
-            if (PosBushValid(x+addX, y+addY))
-            {
-                SeedBush(x+addX, y+addY);
-            }
         }
-    }
-
-    public static Quaternion RandomRotation()
-    {
-        return Quaternion.Euler(new Vector3(0,Random.Range(-360, 360), 0));
-    }
-
-    public static void RandomXY(ref float x, ref float y, float distance)
-    {
-        x = Random.value;
-        y = (float) Math.Sqrt(1 - x * x);
-        float coef = Random.Range(distance, distance*2f);
-        x *= coef*(1 - 2 * Random.Range(0, 2));
-        y *= coef*(1 - 2 * Random.Range(0, 2));
-    }
-    public static bool PosTreeValid(float x, float y)
-    {
-        foreach (Vector2 tree in allTrees)
-        {
-            float distance = (x - tree.x) * (x - tree.x) + (tree.y - y) * (tree.y - y);
-            if (x*x+y*y>45000|| distance<30)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
     
-    public static bool PosBushValid(float x, float y)
-    {
-        foreach (Vector2 tree in allTrees)
+        public void trees()
         {
-            float distance = (x - tree.x) * (x - tree.x) + (tree.y - y) * (tree.y - y);
-            if (x*x+y*y>45000 || distance<5)
+            List<float[]> possibleTrees = RandomListXY(); //List of position of all trees (maybe not possible)
+            foreach (float[] possibleTree in possibleTrees)
             {
-                return false;
+                float y = 0f;
+                if (PositionValid(possibleTree, ref y))
+                { 
+                    GameObject tree = Instantiate(RandomTree(), new Vector3(possibleTree[0], y,possibleTree[1]), RandomRotation());
+                }
             }
         }
-        foreach (Vector2 bush in allBushes)
+    
+        public static List<float[]> RandomListXY()
         {
-            float distance = (x - bush.x) * (x - bush.x) + (bush.y - y) * (bush.y - y);
-            if (x*x+y*y>45000 || distance<100)
+            List<float[]> list = new List<float[]>();
+            for (int x = -490; x <= 490; x+=6)
             {
-                return false;
+                for (int z = -490; z <= 490; z+=6)
+                {
+                    float randomX = Random.Range(-2.5f, 2.5f);
+                    float randomZ = Random.Range(-2.5f, 2.5f);
+                    list.Add(new float[] {x+randomX, z+randomZ});
+                }
+            }
+            return list;
+        }
+        public static bool PositionValid(float[] possibleTree,ref float y)
+        {
+            RaycastHit hit;
+            float x = possibleTree[0];
+            float z = possibleTree[1];
+            if (Physics.Raycast(new Vector3(x, 100, z), Vector3.down, out hit))
+            {
+                if (hit.transform.tag == "mapFloor")
+                {
+                    y = hit.point.y;
+                    Vector3 direction = hit.normal;
+                    if (direction.y > 0.95f)
+                    {
+                        return true;
+                    }
+                }
+            }
+    
+            return false;
+        }
+    
+        public static Quaternion RandomRotation()
+        {
+            return Quaternion.Euler(new Vector3(0,Random.Range(-360, 360), 0));
+        }
+    
+        public GameObject RandomTree()
+        {
+            if (Random.value < 0.7f)
+            {
+                return tree1;
+            }
+            else
+            {
+                return tree2;
             }
         }
-
-        return true;
-    }
-
-    public GameObject RandomTree()
-    {
-        if (Random.value < 0.7f)
-        {
-            return tree1;
-        }
-        else
-        {
-            return tree2;
-        }
-    }
 }
