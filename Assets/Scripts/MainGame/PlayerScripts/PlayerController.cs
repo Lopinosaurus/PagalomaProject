6b5ccc0fb6653a1464a162c2303f8a3123514dc0
paul.haardt <paul.haardt@epita.fr>
2022-03-02 23:36:37 +0100

    Revert "ignore this ! this is a test where i deleted crucial files !"
    This reverts commit d93ca45b0d6ec8c0f8fdcb0dde79c5a8a9e92190.

---
+++ Assets/Scripts/MainGame/PlayerScripts/PlayerController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using Photon.Pun;
+// ReSharper disable All
+
+[RequireComponent(typeof(PlayerAnimation))]
+/*
+ //TODO implement CharacterController
+[RequireComponent(typeof(CharacterController))]
+*/
+
+public class PlayerController : MonoBehaviour
+{
+    #region Attributes
+    
+    // Movement components
+    private Rigidbody _rigidBody;
+    private CharacterController _characterController;
+    
+    // Network component
+    private PhotonView _photonView;
+    
+    // Sub scripts
+    private PlayerMovement _playerMovement;
+    private PlayerLook _playerLook;
+    private PlayerAnimation _playerAnimation;
+    
+    // Miscellaneous
+
+    #endregion
+
+    #region Unity methods
+    
+    private void Awake()
+    {
+        // Movement components
+        _rigidBody = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
+        
+        // Network component
+        _photonView = GetComponent<PhotonView>();
+        
+        // Sub scripts
+        _playerMovement = GetComponent<PlayerMovement>();
+        _playerLook = GetComponent<PlayerLook>();
+        _playerAnimation = GetComponent<PlayerAnimation>();
+    }
+    
+    private void Start()
+    {
+        if (_photonView.IsMine) return;
+        
+        Destroy(GetComponentInChildren<Camera>().gameObject);
+        Destroy(_rigidBody);
+    }
+    
+    private void Update()
+    {
+        if (!_photonView.IsMine) return;
+        
+        _playerLook.Look();
+        
+        _playerMovement.Move();
+
+        // Updates the appearance based on the MovementType
+        _playerAnimation.UpdateAppearance(_playerMovement.currentMovementType);
+    }
+    
+    private void FixedUpdate()
+    {
+        if (!_photonView.IsMine) return;
+        
+        //TODO
+        // Will soon be improved to remove jittering
+        _rigidBody.MovePosition(_rigidBody.position + transform.TransformDirection(_playerMovement.moveAmount) * Time.fixedDeltaTime);
+        
+        UpdateHitbox();
+    }
+    
+    #endregion
+
+    private void UpdateHitbox() => _playerAnimation.UpdateHitbox(_playerMovement.currentMovementType);
+
+
+    // Network syncronization
+    #region RPCs
+
+    [PunRPC]
+    // Syncronizes the appearance
+    void RPC_UpdateAppearance(PlayerMovement.MovementTypes movementType) => _playerAnimation.UpdateAppearance(_playerMovement.currentMovementType);
+
+    #endregion
+}
