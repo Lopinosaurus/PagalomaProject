50f7a1333cca1868f9133c02cbc7bc04ec7417a4
Louis Laverne <louis.laverne@epita.fr>
2022-03-04 17:28:00 +0100

    First push Louis

---
+++ Assets/Scripts/MainMenu/PlayMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayMenu : MonoBehaviour
+{
+    
+    public void Quit()
+    {
+        Debug.Log("Leaving Game...");
+        Application.Quit();
+    }
+    
+    
+    
+}
