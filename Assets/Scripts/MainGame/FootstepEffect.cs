a3b04d1a77549050ac28a61beb249fcaa49bb5ab
Louis Laverne <louis.laverne@epita.fr>
2022-03-12 20:29:41 +0100

    Push Stable Louis - Addition du Sons

---
+++ Assets/Scripts/MainGame/FootstepEffect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepEffect : MonoBehaviour
+{
+    
+    [SerializeField] CharacterController characterController;
+    [SerializeField] AudioSource footsteps;
+
+
+    
+    void Update()
+    {
+        if (characterController.isGrounded && footsteps.isPlaying == false && characterController.velocity.magnitude > 0f)
+
+        {
+            footsteps.volume = Random.Range(0.8f, 1);
+            footsteps.pitch = Random.Range(0.8f, 1.1f);
+            footsteps.Play();
+        }
+    }
+}
