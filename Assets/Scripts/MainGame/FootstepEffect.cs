7f18639e707bd4531152ffe03637ee011641da7b
Louis Laverne <louis.laverne@epita.fr>
2022-05-16 13:28:57 +0200

    Stable - Footsteps Added

--- Assets/Scripts/MainGame/FootstepEffect.cs
+++ Assets/Scripts/MainGame/FootstepEffect.cs
@@ -6,19 +6,98 @@ public class FootstepEffect : MonoBehaviour
 {
     
     [SerializeField] CharacterController characterController;
-    [SerializeField] AudioSource footsteps;
-
+    public AudioSource footsteps;
+    [SerializeField] AudioClip dryCrouching;
+    [SerializeField] AudioClip dryWalking;
+    [SerializeField] AudioClip drySprinting;
+    [SerializeField] AudioClip wetCrouching;
+    [SerializeField] AudioClip wetWalking;
+    [SerializeField] AudioClip wetSprinting;
 
     
+
     void Update()
     {
-        if (characterController == null) return; // Avoid error on CharacterController destruction
-        if (characterController.isGrounded && footsteps.isPlaying == false && characterController.velocity.magnitude > 0f)
+        // Avoid error on CharacterController destruction
+        if (characterController == null) 
+            return;
+
+        #region Crouching Case
+        
+        if (characterController.isGrounded 
+            && (!footsteps.isPlaying) && characterController.velocity.magnitude > 0f 
+            && characterController.velocity.magnitude < 1.5f)
 
         {
-            footsteps.volume = Random.Range(0.8f, 1);
-            footsteps.pitch = Random.Range(0.8f, 1.1f);
-            footsteps.Play();
+            int soundTaker = Random.Range(1, 2);
+
+            if (1 == soundTaker)
+            {
+                footsteps.clip = dryCrouching;
+                footsteps.volume = Random.Range(0.8f, 1);
+                footsteps.pitch = Random.Range(0.8f, 1.1f);
+                footsteps.Play();
+            }
+
+            else
+            {
+                footsteps.clip = wetCrouching;
+                footsteps.volume = Random.Range(0.8f, 1);
+                footsteps.pitch = Random.Range(0.8f, 1.1f);
+                footsteps.Play();
+            }
         }
+
+        #endregion
+
+        #region Walking Case
+        else if (characterController.isGrounded 
+            && (!footsteps.isPlaying) && characterController.velocity.magnitude > 0f 
+            && characterController.velocity.magnitude < 3f)
+        {
+            int soundTaker = Random.Range(1, 2);
+
+            if (1 == soundTaker)
+            {
+                footsteps.clip = dryWalking;
+                footsteps.volume = Random.Range(0.8f, 1);
+                footsteps.pitch = Random.Range(0.8f, 1.1f);
+                footsteps.Play();
+            }
+
+            else
+            {
+                footsteps.clip = wetWalking;
+                footsteps.volume = Random.Range(0.8f, 1);
+                footsteps.pitch = Random.Range(0.8f, 1.1f);
+                footsteps.Play();
+            }
+        }
+        #endregion
+
+        #region Sprinting Case
+        else if (characterController.isGrounded
+            && (!footsteps.isPlaying) && characterController.velocity.magnitude > 0f
+            && characterController.velocity.magnitude < 6f)
+        {
+            int soundTaker = Random.Range(1, 2);
+
+            if (1 == soundTaker)
+            {
+                footsteps.clip = drySprinting;
+                footsteps.volume = Random.Range(0.8f, 1);
+                footsteps.pitch = Random.Range(0.8f, 1.1f);
+                footsteps.Play();
+            }
+
+            else
+            {
+                footsteps.clip = wetSprinting;
+                footsteps.volume = Random.Range(0.8f, 1);
+                footsteps.pitch = Random.Range(0.8f, 1.1f);
+                footsteps.Play();
+            }
+        }
+        #endregion
     }
 }
