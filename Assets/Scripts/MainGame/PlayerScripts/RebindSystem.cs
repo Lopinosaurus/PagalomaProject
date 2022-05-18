23a17826fe02d3bdfce7ec690920ea5ed0ab354f
Louis Laverne <louis.laverne@epita.fr>
2022-05-18 16:39:27 +0200

    Intermediate commit to pull

--- Assets/Scripts/MainGame/PlayerScripts/RebindSystem.cs
+++ Assets/Scripts/MainGame/PlayerScripts/RebindSystem.cs
@@ -4,35 +4,89 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine.UI;
 
 public class RebindSystem : MonoBehaviour
 {
-    private PlayerInput _playerInput;
-    [SerializeField] private GameObject startRebindObject = null;
-    [SerializeField] private GameObject waitingForInputObject = null;
+    [SerializeField] private InputActionAsset asset;
     private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
-
-
-    private void Awake()
-    {
-        _playerInput = GetComponent<PlayerInput>();
-    }
+    
 
     public void RebindJump()
     {
-        startRebindObject.SetActive(false);
-        waitingForInputObject.SetActive(true);
-        
-        rebindingOperation = _playerInput.actions["Jump"].PerformInteractiveRebinding()
+        rebindingOperation = asset.actionMaps[0].FindAction("Jump").PerformInteractiveRebinding()
             .WithControlsExcluding("Mouse")
             .WithControlsExcluding("Escape")
             .OnMatchWaitForAnother(.1f)
             .OnComplete(operation =>
             {
                 rebindingOperation.Dispose();
-                startRebindObject.SetActive(true);
-                waitingForInputObject.SetActive(false);
-            });
+            })
+            .Start();
+    }
+    
+    public void RebindSprint()
+    {
+        rebindingOperation = asset.actionMaps[0].FindAction("Sprint").PerformInteractiveRebinding()
+            .WithControlsExcluding("Mouse")
+            .WithControlsExcluding("Escape")
+            .OnMatchWaitForAnother(.1f)
+            .OnComplete(operation =>
+            {
+                rebindingOperation.Dispose();
+            })
+            .Start();
+    }
+    
+    public void RebindCrouch()
+    {
+        rebindingOperation = asset.actionMaps[0].FindAction("Crouch").PerformInteractiveRebinding()
+            .WithControlsExcluding("Mouse")
+            .WithControlsExcluding("Escape")
+            .OnMatchWaitForAnother(.1f)
+            .OnComplete(operation =>
+            {
+                rebindingOperation.Dispose();
+            })
+            .Start();
+    }
+    
+    public void RebindKill()
+    {
+        rebindingOperation = asset.actionMaps[0].FindAction("Kill").PerformInteractiveRebinding()
+            .WithControlsExcluding("Mouse")
+            .WithControlsExcluding("Escape")
+            .OnMatchWaitForAnother(.1f)
+            .OnComplete(operation =>
+            {
+                rebindingOperation.Dispose();
+            })
+            .Start();
+    }
+    
+    public void RebindClick()
+    {
+        rebindingOperation = asset.actionMaps[0].FindAction("Click").PerformInteractiveRebinding()
+            .WithControlsExcluding("Escape")
+            .OnMatchWaitForAnother(.1f)
+            .OnComplete(operation =>
+            {
+                rebindingOperation.Dispose();
+            })
+            .Start();
+    }
+    
+    public void RebindMove()
+    {
+        rebindingOperation = asset.actionMaps[0].FindAction("Move").PerformInteractiveRebinding()
+            .WithControlsExcluding("Escape")
+            .WithControlsExcluding("Mouse")
+            .OnMatchWaitForAnother(.1f)
+            .OnComplete(operation =>
+            {
+                rebindingOperation.Dispose();
+            })
+            .Start();
     }
 }
