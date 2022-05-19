d1697d86367195470ce019561dd4671b41587eec
Paul Haardt <paul.haardt@epita.fr>
2022-05-19 16:13:28 +0200

    fixed some glitches and hitboxes

--- Assets/Scripts/MainGame/DecorsInteraction/Door.cs
+++ Assets/Scripts/MainGame/DecorsInteraction/Door.cs
@@ -10,14 +10,14 @@ public class Door : MonoBehaviour
     public GameObject message;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<PhotonView>().IsMine)
+        if (other.GetType() == typeof(CharacterController) && other.GetComponent<PhotonView>().IsMine)
         {
             other.GetComponent<PlayerInteraction>().NearDoor(message, gameObject, true);
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<PhotonView>().IsMine)
+        if (other.GetType() == typeof(CharacterController) && other.GetComponent<PhotonView>().IsMine)
         {
             other.GetComponent<PlayerInteraction>().NearDoor(message, gameObject, false);
         }
