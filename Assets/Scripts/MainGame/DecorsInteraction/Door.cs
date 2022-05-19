d40a752aaa8de41192da0980bbc2d6cfd9a77e1a
dausyla <dausyla@gmail.com>
2022-05-19 10:54:29 +0200

    door message fixed

--- Assets/Scripts/MainGame/DecorsInteraction/Door.cs
+++ Assets/Scripts/MainGame/DecorsInteraction/Door.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using Photon.Pun;
 using UnityEngine;
 
 public class Door : MonoBehaviour
@@ -9,10 +10,16 @@ public class Door : MonoBehaviour
     public GameObject message;
     private void OnTriggerEnter(Collider other)
     {
-        other.GetComponent<PlayerInteraction>().NearDoor(message, gameObject, true);
+        if (other.GetComponent<PhotonView>().IsMine)
+        {
+            other.GetComponent<PlayerInteraction>().NearDoor(message, gameObject, true);
+        }
     }
     private void OnTriggerExit(Collider other)
     {
-        other.GetComponent<PlayerInteraction>().NearDoor(message, gameObject, false);
+        if (other.GetComponent<PhotonView>().IsMine)
+        {
+            other.GetComponent<PlayerInteraction>().NearDoor(message, gameObject, false);
+        }
     }
 }
