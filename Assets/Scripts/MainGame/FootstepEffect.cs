157b12074ee514ab39e32b4161bf3468341947a3
Louis Laverne <louis.laverne@epita.fr>
2022-05-28 17:37:11 +0200

    Intermediate commit to pull

--- Assets/Scripts/MainGame/FootstepEffect.cs
+++ Assets/Scripts/MainGame/FootstepEffect.cs
@@ -9,36 +9,45 @@ public class FootstepEffect : MonoBehaviour
 {
     [SerializeField] private float velocityMagnitude;
     [SerializeField] private AudioClip dryFootstep;
+    [SerializeField] private CharacterController characterController;
     [SerializeField] private AudioSource plyAudioSource;
     [SerializeField, Range(.1f, 10)] private float maxDistance;
     [SerializeField] private float playerDistanceCounter;
-    private PlayerAnimation playerAnim;
+    [SerializeField] private Vector3 velocity3D;
+    [SerializeField] private Vector2 velocity2Draw;
+    private Animator animator;
+    private PlayerAnimation playerAnimation;
 
     void Start()
     {
         playerDistanceCounter = 0;
-        playerAnim = GetComponent<PlayerAnimation>();
         plyAudioSource.playOnAwake = false;
         plyAudioSource.volume = .2f;
+        animator = GetComponent<Animator>();
+        playerAnimation = GetComponent<PlayerAnimation>();
     }
     
-    void FixedUpdate()
-    {
-        velocityMagnitude = playerAnim.Velocity2DMagnitude;
-        
-        playerDistanceCounter += velocityMagnitude * Time.fixedDeltaTime;
-        if (playerDistanceCounter >= maxDistance)
-        {
-            playerDistanceCounter = 0;
-            plyAudioSource.clip = dryFootstep;
-            plyAudioSource.Play();
-        }
-        
-        // Yes, it's realistic.
-        if (0 == velocityMagnitude)
-        {
-            plyAudioSource.Stop();    
-        }
-        
-    }
+     void FixedUpdate()
+     {
+         velocity2Draw = new Vector2
+         {
+             x = animator.GetFloat(playerAnimation._velocityXHash),
+             y = animator.GetFloat(playerAnimation._velocityZHash)
+         };
+         
+         velocityMagnitude = velocity2Draw.magnitude;
+         playerDistanceCounter += velocityMagnitude * Time.fixedDeltaTime;
+         if (playerDistanceCounter >= maxDistance)
+         {
+             playerDistanceCounter = 0;
+             plyAudioSource.clip = dryFootstep;
+             plyAudioSource.Play();
+         }
+         
+         // Yes, it's realistic.
+         if (0 == velocityMagnitude)
+         {
+             plyAudioSource.Stop();    
+         }
+     }
 }
