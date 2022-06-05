d0a6173b81358790cbf82809517b2aa66008c97e
PaulHaardt <97391871+PaulHaardt@users.noreply.github.com>
2022-06-05 19:34:45 +0200

    death anm moved to global

--- Assets/Scripts/MainGame/PlayerScripts/Roles/Role.cs
+++ Assets/Scripts/MainGame/PlayerScripts/Roles/Role.cs
@@ -132,6 +132,8 @@ namespace MainGame.PlayerScripts.Roles
                 _playerInput.actions["Kill"].Disable();
             }
             
+            _playerAnimation.EnableDeathAppearance();
+            
             // Prevents the head for rotating in the ground when dying
             _rotationConstraint.enabled = false;
 
@@ -144,7 +146,7 @@ namespace MainGame.PlayerScripts.Roles
             VoteMenu.Instance.UpdateVoteItems();
 
             // Initial camera position
-            if ((bool)_cameraHolder)
+            if (null != _cameraHolder)
             {
                 Vector3 startingPos = _cameraHolder.transform.position;
                 Vector3 endingPos = new Vector3
@@ -153,9 +155,7 @@ namespace MainGame.PlayerScripts.Roles
                     y = startingPos.y + maxDeathCamDistance,
                     z = startingPos.z
                 };
-
-                _playerAnimation.EnableDeathAppearance();
-
+                
                 // Final camera position
                 if (Physics.Raycast(startingPos, Vector3.up, out RaycastHit hitInfo, maxDeathCamDistance))
                     endingPos.y = hitInfo.point.y - 0.2f;
