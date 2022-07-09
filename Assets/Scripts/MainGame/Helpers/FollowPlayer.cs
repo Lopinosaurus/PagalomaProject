2d3eb36f7d3724e273eae30c26f79603d4ab5eba
PaulHaardt <97391871+PaulHaardt@users.noreply.github.com>
2022-07-10 01:37:32 +0200

    quest in progress

--- Assets/Scripts/MainGame/Helpers/FollowPlayer.cs
+++ Assets/Scripts/MainGame/Helpers/FollowPlayer.cs
@@ -1,31 +1,62 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace MainGame.Helpers
 {
     public class FollowPlayer : ScriptableObject
     {
-        public SphereCollider _goodRadiusCollider, _warningRadiusCollider, _tooFarRadiusCollider;
+        public struct RangeCollider
+        {
+            public float radius
+            {
+                get => collider.radius;
+                set => collider.radius = value;
+            }
+            public SphereCollider collider;
+            public bool isColliding;
+            public Quest _quest;
+        }
+
+        public RangeCollider goodRadiusCollider, warningRadiusCollider;
         public float _goodRadius;
         public float _warningRadius;
-        public float _tooFarRadius;
 
-        public void SetupPlayer(GameObject playerToFollow)
+        public void SetupPlayerColliders(GameObject playerToFollow)
         {
             GameObject go = new GameObject("QuestColliders");
             go.transform.SetParent(playerToFollow.transform);
 
-            AddCollider(go, _goodRadius, "goodRadius");
-            AddCollider(go, _warningRadius, "warningRadius");
-            AddCollider(go, _tooFarRadius, "tooFarRadius");
+            AddCollider(go, _goodRadius, "goodRadius", out goodRadiusCollider);
+            AddCollider(go, _warningRadius, "warningRadius", out warningRadiusCollider);
         }
 
-        private static void AddCollider(GameObject gameObjectToAddColliderTo, float radius, string name_)
+        private static void AddCollider(GameObject gameObjectToAddColliderTo, float radius, string name_, out RangeCollider rangeCollider)
         {
             GameObject go = new GameObject(name_);
             go.transform.SetParent(gameObjectToAddColliderTo.transform);
             SphereCollider goCollider = go.AddComponent<SphereCollider>();
-            goCollider.radius = radius;
-            goCollider.isTrigger = true;
+
+            rangeCollider = new RangeCollider
+            {
+                collider = goCollider,
+                radius = radius
+            };
+        }
+    }
+
+    public class UpdateCollision : MonoBehaviour
+    {
+        private FollowPlayer.RangeCollider _rangeCollider;
+        private QuestManager.Quest _quest;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            _rangeCollider.isColliding = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            _rangeCollider.isColliding = false;
         }
     }
 }
